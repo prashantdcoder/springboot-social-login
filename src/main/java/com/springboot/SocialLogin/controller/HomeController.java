75fb9eae005cfaf0b58e19b780692d88ee7bca50
unknown <prashantdcoder@gmail.com>
2019-01-26 22:35:10 +0530

    Login form

--- src/main/java/com/springboot/SocialLogin/controller/HomeController.java
+++ src/main/java/com/springboot/SocialLogin/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.springboot.SocialLogin.controller;
 
+import com.springboot.SocialLogin.controller.service.HomeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,8 +10,16 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(value = "/home")
 public class HomeController {
 
+    @Autowired
+    HomeService homeService;
+
     @RequestMapping(value = "/index")
     public ModelAndView index() {
+        return new ModelAndView("index");
+    }
+
+    @RequestMapping(value = "/facebook")
+    public ModelAndView facebook(){
         return new ModelAndView("index");
     }
 }
---
+++ src/main/java/com/springboot/SocialLogin/controller/service/HomeService.java
@@ -0,0 +1,12 @@
+package com.springboot.SocialLogin.controller.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HomeService {
+
+    public void facebookLogin(){
+
+
+    }
+}
